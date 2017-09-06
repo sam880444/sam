c5f2083aae53a6194f175712aab3b678e0564d22
jimmy36370120 <jimmy3637@gmail.com>
2017-09-06 10:54:20 +0800

    Initial Commit

---
+++ Myswift02/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Myswift02
+//
+//  Created by user17 on 2017/9/6.
+//  Copyright © 2017年 user_17demodemo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var a = Int.max
+print(a)
+var b = a - 1
+print(b)
